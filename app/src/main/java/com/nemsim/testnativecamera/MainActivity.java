3fb010cd8785411af424ac8cdcc789ed3905372e
Nem <nem@nemsim.com>
2015-12-22 10:31:57 -0600

    wee

--- app/src/main/java/com/nemsim/testnativecamera/MainActivity.java
+++ app/src/main/java/com/nemsim/testnativecamera/MainActivity.java
@@ -3,14 +3,24 @@ package com.nemsim.testnativecamera;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCamera = getCameraInstance();
+
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
 
     public static Camera getCameraInstance() {
