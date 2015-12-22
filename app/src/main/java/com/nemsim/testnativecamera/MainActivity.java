df2ea90d53a1e1d1ef51037fdcfa5044421a7453
Nem <nem@nemsim.com>
2015-12-22 10:27:49 -0600

    adding camera preview

--- app/src/main/java/com/nemsim/testnativecamera/MainActivity.java
+++ app/src/main/java/com/nemsim/testnativecamera/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nemsim.testnativecamera;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e){
+            // Toast.makeText(this.getApplicationContext(), "Can't get camera", Toast.LENGTH_LONG).show();
+        }
+
+        return c;
     }
 }
